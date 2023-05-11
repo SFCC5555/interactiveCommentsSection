3f74c61fd6a022749cf0b6299240a676bffdfed6
sfcc5555 <sfcc.5555@gmail.com>
2023-05-10 21:16:27 -0500

    a

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({handleClick,color,text}) => {
   return (
-    <button className='hover:opacity-80 px-2 py-1 sm:px-4 sm:py-2 rounded-md' onClick={handleClick} style={{color:'var(--white)',backgroundColor:color}}>{text}</button>
+    <button className='hover:opacity-80 px-4 py-2 rounded-md' onClick={handleClick} style={{color:'var(--white)',backgroundColor:color}}>{text}</button>
   )
 }
 

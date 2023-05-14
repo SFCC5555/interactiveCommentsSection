884eca405fbf73ec22b46189323415853a8b63f1
sfcc5555 <sfcc.5555@gmail.com>
2023-05-13 20:12:45 -0500

    a

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -28,7 +28,7 @@ const NavBar = () => {
      <nav className='fixed px-5 flex items-center justify-between w-full h-14 top-0 left-0 z-10 font-medium' style={{backgroundColor:'var(--moderateBlue)',color:'var(--white)'}}>
         <div>The Social Network</div>
         <div onClick={renderUsersMenu} className='flex items-center gap-2 cursor-pointer hover:opacity-80'>
-            <div id='userName'>{currentUser.username}</div>
+            <div className='text-sm sm:text-base' id='userName'>{currentUser.username}</div>
             <div className='w-10 h-10 overflow-hidden rounded-full'>
               <img id='userImage' className='w-10' src={currentUser.image.png} alt={currentUser.username}/>
             </div>
